3d64972270e1bae45f0d822a4cf3e681d3c21d91
Michael Williamson <mike@zwobble.org>
2013-05-23 23:08:19 +0100

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='marmoset',
-    version='0.1.0',
+    version='0.1.1',
     description='Convert Python values, such as lists and dicts, into human-friendly strings',
     long_description=read("README"),
     author='Michael Williamson',
